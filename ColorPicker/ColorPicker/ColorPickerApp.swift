c4a619855da84f17b744cb4d584642e6f4234564
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2023-12-13 17:39:56 +0300

    project created

---
+++ ColorPicker/ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Roman Litvinovich on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
